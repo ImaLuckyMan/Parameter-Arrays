07c68605743c5af54d8ce61d61f6f724309a81c4
ImaLuckyMan <Ima_Lucky_Man@yahoo.com>
2020-02-29 10:30:59 -0800

    Add project files.

---
+++ ParamsArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // Writes the exeption from the Util class to the screen picking up an empty array. This tests the 2nd excetion.
+            //Console.WriteLine(Util.Sum()); // Had to comment out because otherwise the program won't continue.
+            // Writes the exception from the Util class to the screen if value equals null. This tests the 1st exception.
+            //Console.WriteLine(Util.Sum(null)); // Had to comment out because otherwise the program won't continue.
+            // Writes the total of the summed array ensuring the class method is working correctly. 
+            //Console.WriteLine(Util.Sum(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)); // Expected total 55 from the paramList.
+            Console.WriteLine(Util.Sum(2, 4, 6, 8, 10)); /* Also works with only 3 arguments using the optional params class method in Util. but if you give it 5, the compiler 
+            chooses the parameter array becuase there are only 4 optional params declared in the Util class.*/
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ ParamsArray/Util.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Util
+    {
+        //Going to Sum the parameters in the Parameter list as long as the conditions are met.
+        public static int Sum(params int[] paramList)
+        {
+            Console.WriteLine("Using parameter list");
+            // Checks to ensure the parameter list is not null. If it is, it throws an exception stating so.
+            if (paramList == null)
+            {
+                throw new ArgumentException("Util.Sum: null parameter list");
+            }
+            // Checks to ensure the parameter list length does not equal zero. If it is, it throws an empty exception stating so.
+            if (paramList.Length == 0)
+            {
+                throw new ArgumentException("Util.Sum: empty parameter list");
+            }
+
+            int sumTotal = 0; // declares a variable to hold the sum and instantiates it with zero.
+            foreach (int i in paramList) // checks each iteration or index in the array and sums them as long as they meet the above criteria.
+            {
+                sumTotal += i; // adds each index to sumTotal
+            }
+            return sumTotal; // returns the total of the added array.
+        }
+        public static int Sum(int param1 = 0, int param2 = 0, int param3 = 0, int param4 = 0)
+        {
+            Console.WriteLine("Using optional parameters");
+            int sumTotal = param1 + param2 + param3 + param4;
+            return sumTotal;
+        }
+
+    }
+}
